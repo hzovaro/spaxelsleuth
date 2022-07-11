e8a0e9e8a9558aef10fa798e118c2cf0c0460d61
Henry Zovaro <henry.zovaro@anu.edu.au>
2022-07-11 11:17:48 +1000

    Last commit before replacing with contents of updated make_dfs_lzifu.py

--- loaddata/lzifu.py
+++ loaddata/lzifu.py
@@ -1,120 +1,21 @@
-import os
-import pandas as pd
-import numpy as np
-from astropy.io import fits
-from tqdm import tqdm
+"""
+File:       lzifu.py
+Author:     Henry Zovaro
+Email:      henry.zovaro@anu.edu.au
 
-from spaxelsleuth.loaddata import dqcut, linefns
+DESCRIPTION
+------------------------------------------------------------------------------
+This script contains the functions 
 
-sami_data_path = "/priv/meggs3/u5708159/SAMI/sami_dr3/"
-sami_datacube_path = "/priv/myrtle1/sami/sami_data/Final_SAMI_data/cube/sami/dr3/"
-lzifu_data_path = "/priv/meggs3/u5708159/LZIFU/products/"
+load_lzifu_galaxies():
+    Equivalent to load_sami_galaxies() in sami.py. This function returns the 
+    Pandas DataFrames containing spaxel-by-spaxel information for SAMI galaxies 
+    that were created in make_dfs_lzifu.py. The DataFrame corresponding to a 
+    single galaxy can be returned, or the entire subset of high-S/N galaxies,
+    which includes 1- or 0-component-only galaxies from the original SAMI
+    dataset.
 
-#######################################################################
-def load_lzifu_galaxies(ncomponents, bin_type,
-                        eline_SNR_min,
-                        gal=None,
-                        eline_list=["HALPHA", "HBETA", "OIII5007", "OI6300", "NII6583", "SII6716", "SII6731"],
-                        sigma_gas_SNR_cut=True, sigma_gas_SNR_min=3, stekin_cut=True,
-                        vgrad_cut=False, correct_extinction=False):
-    """
-    load_lzifu_galaxies(): load a DataFrame containing emission line fluxes,
-    etc. for a galaxy (or galaxies) re-processed using LZIFU.
-
-    If gal is specified, then the DataFrame for that particular galaxy is 
-    returned. Otherwise, a DataFrame containing ALL galaxies in the high-S/N 
-    subset of LZIFU galaxies (as defined in make_dfs_lzifu.py) is returned.
-
-    """
-    #######################################################################
-    # INPUT CHECKING
-    #######################################################################
-    assert ncomponents == "recom", "ncomponents must be 'recom'!!"
-    assert bin_type == "default", "bin_type must be 'default' for now!!"
-    assert eline_SNR_min >= 0, "eline_SNR_min must be positive!"
-    assert sigma_gas_SNR_min >= 0, "sigma_gas_SNR_min must be positive!"
-
-    if gal is None:
-        print(f"Loading LZIFU DataFrame for all galaxies in the LZIFU subsample...")
-        df_fname = f"lzifu_subsample.hd5"
-    else:
-        print(f"Loading LZIFU DataFrame for galaxy {gal}...")
-        df_fname = f"lzifu_{gal}.hd5"
-    assert os.path.exists(os.path.join(sami_data_path, df_fname)),\
-        f"File {os.path.join(sami_data_path, df_fname)} does does not exist!"
-    
-    #######################################################################
-    # LOAD THE DATAFRAME FROM MEMORY
-    #######################################################################
-    df = pd.read_hdf(os.path.join(sami_data_path, df_fname))
-
-    ######################################################################
-    # DQ and S/N CUTS
-    ######################################################################
-    df = dqcut.dqcut(df=df, ncomponents=3,
-                  eline_SNR_min=eline_SNR_min, eline_list=eline_list,
-                  sigma_gas_SNR_cut=sigma_gas_SNR_cut, sigma_gas_SNR_min=sigma_gas_SNR_min, 
-                  sigma_inst_kms=29.6,
-                  vgrad_cut=vgrad_cut,
-                  stekin_cut=stekin_cut)
-
-    ######################################################################
-    # EVALUATE LINE RATIOS & SPECTRAL CLASSIFICATIONS
-    ######################################################################
-    df = linefns.ratio_fn(df, s=f" (total)")
-    df = linefns.bpt_fn(df, s=f" (total)")
-    df = linefns.law2021_fn(df, s=f" (total)")
-    for ii in range(3):
-        df = linefns.ratio_fn(df, s=f" (component {ii})")
-        df = linefns.bpt_fn(df, s=f" (component {ii})")
-        df = linefns.law2021_fn(df, s=f" (component {ii})")
-
-    ######################################################################
-    # EVALUATE ADDITIONAL COLUMNS - log quantites, etc.
-    ######################################################################
-    df = dqcut.compute_extra_columns(df, ncomponents=3 if ncomponents=="recom" else 1)
-
-    ######################################################################
-    # WHAV* classification
-    ######################################################################
-    df = linefns.whav_fn(df, ncomponents=3 if ncomponents=="recom" else 1)
-
-    ######################################################################
-    # CORRECT HALPHA FLUX AND EW FOR EXTINCTION
-    # Note that we only have enough information from the provided extinction
-    # maps to correct the Halpha line. We therefore apply this correction 
-    # AFTER we compute line ratios, etc.
-    ######################################################################
-    if correct_extinction:
-        print("WARNING: in load_lzifu_galaxy: correcting Halpha and HALPHA EW for extinction!")
-        # Use the provided extinction correction map to correct Halpha 
-        # fluxes & EWs
-        df["HALPHA (total)"] *= df["HALPHA extinction correction"]
-        df["HALPHA error (total)"] *= df["HALPHA extinction correction"]
-        df["HALPHA EW (total)"] *= df["HALPHA extinction correction"]
-        df["HALPHA EW error (total)"] *= df["HALPHA extinction correction"]
-        df["log HALPHA EW (total)"] += np.log10(df["HALPHA extinction correction"])
-        # Re-compute log errors
-        df["log HALPHA EW error (lower) (total)"] = df["log HALPHA EW (total)"] - np.log10(df["HALPHA EW (total)"] - df["HALPHA EW error (total)"])
-        df["log HALPHA EW error (upper) (total)"] = np.log10(df["HALPHA EW (total)"] + df["HALPHA EW error (total)"]) -  df["log HALPHA EW (total)"]
-
-        for component in range(3 if ncomponents == "recom" else 1):
-            df[f"HALPHA (component {component})"] *= df["HALPHA extinction correction"]
-            df[f"HALPHA error (component {component})"] *= df["HALPHA extinction correction"]
-            df[f"HALPHA EW (component {component})"] *= df["HALPHA extinction correction"]
-            df[f"HALPHA EW error (component {component})"] *= df["HALPHA extinction correction"]
-            df[f"log HALPHA EW (component {component})"] += np.log10(df["HALPHA extinction correction"])
-            # Re-compute log errors
-            df[f"log HALPHA EW error (lower) (component {component})"] = df[f"log HALPHA EW (component {component})"] - np.log10(df[f"HALPHA EW (component {component})"] - df[f"HALPHA EW error (component {component})"])
-            df[f"log HALPHA EW error (upper) (component {component})"] = np.log10(df[f"HALPHA EW (component {component})"] + df[f"HALPHA EW error (component {component})"]) -  df[f"log HALPHA EW (component {component})"]
-    else:
-        print("WARNING: in load_lzifu_galaxy: NOT correcting Halpha and HALPHA EW for extinction!")
-    
-    return df
-
-###############################################################################
-def merge_datacubes(gal=None, plotit=False):
-    """
+merge_datacubes():
     By default, LZIFU uses the "likelihood ratio test (LRT)" to determine the 
     optimal number of components in each spaxel. However, this technique often
     produces results very inconsistent with those derived using LZCOMP, the 
@@ -128,9 +29,132 @@ def merge_datacubes(gal=None, plotit=False):
     extensions and data formats as those in <gal>_merge_comp.fits, which is 
     the default output of LZIFU.   
 
+
+PREREQUISITES
+------------------------------------------------------------------------------
+SAMI_DIR and LZIFU_PRODUCTS_PATH must be defined as an environment variable,
+where LZIFU_PRODUCTS_PATH points to the location of the LZIFU output files 
+(e.g. <gal>_1_comp.fits, etc.)
+
+LZIFU must have been run for all galaxies specified. 
+
+Both make_dfs_lzifu.py and make_sami_metadata_df.py must be run first.
+
+------------------------------------------------------------------------------
+Copyright (C) 2022 Henry Zovaro
+"""
+#######################################################################
+import os
+import pandas as pd
+import numpy as np
+from astropy.io import fits
+from tqdm import tqdm
+
+assert "SAMI_DIR" in os.environ, "Environment variable SAMI_DIR is not defined!"
+sami_data_path = os.environ["SAMI_DIR"]
+assert "LZIFU_PRODUCTS_PATH" in os.environ, "Environment variable LZIFU_PRODUCTS_PATH is not defined!"
+lzifu_products_path = os.environ["LZIFU_PRODUCTS_PATH"]
+assert "LZIFU_PRODUCTS_PATH" in os.environ, "Environment variable LZIFU_PRODUCTS_PATH is not defined!"
+lzifu_products_path = os.environ["LZIFU_PRODUCTS_PATH"]
+
+#######################################################################
+def load_lzifu_galaxies(ncomponents, bin_type, correct_extinction, eline_SNR_min,
+                        gal=None, debug=False):
+    """
+    load a DataFrame containing emission line fluxes, etc. for a galaxy 
+    (or galaxies) re-processed using LZIFU.
+
+    INPUTS
+    ---------------------------------------------------------------------------
+    ncomponents:        str
+        Number of components; may either be "1" (corresponding to the 
+        1-component Gaussian fits) or "recom" (corresponding to the multi-
+        component Gaussian fits).
+
+    bin_type:           str
+        Binning scheme used. Must be one of 'default' or 'adaptive' or 
+        'sectors'.
+
+    correct_extinction: bool
+        If True, load the DataFrame in which the emission line fluxes (but not 
+        EWs) have been corrected for intrinsic extinction.
+
+    eline_SNR_min:      int 
+        Minimum flux S/N to accept. Fluxes below the threshold (plus associated
+        data products) are set to NaN.
+
+    gal:                int
+        GAMA ID of the galaxy to load. If not specified, the DataFrame 
+        containing all galaxies meeting the S/N criterion in make_dfs_lzifu.py 
+        is returned.
+
+    USAGE
+    ---------------------------------------------------------------------------
+    load_lzifu_galaxies() is called as follows:
+
+        >>> from spaxelsleuth.loaddata.lzifu import load_lzifu_galaxies
+        >>> df = load_lzifu_galaxies(ncomponents, bin_type, correct_extinction, 
+                                     eline_SNR_min, gal, debug)
+
+    If gal is not specified then the DataFrame containing the subset of "high-S/N"
+    galaxies as defined in make_dfs_lzifu.py are returned.
+
+    OUTPUTS
+    ---------------------------------------------------------------------------
+    The Dataframe.
+
+    """
+    #######################################################################
+    # INPUT CHECKING
+    #######################################################################
+    assert (ncomponents == "recom") | (ncomponents == "1"), "ncomponents must be 'recom' or '1'!!"
+    assert bin_type in ["default", "adaptive", "sectors"], "bin_type must be 'default' or 'adaptive' or 'sectors'!!"
+
+    # Input file name
+    if gal is None:
+        print(f"Loading LZIFU DataFrame for all galaxies in the LZIFU subsample...")
+        df_fname = f"lzifu_subsample_{bin_type}_{ncomponents}-comp"
+    else:
+        print(f"Loading LZIFU DataFrame for galaxy {gal}...")
+        df_fname = f"lzifu_{gal}_{bin_type}_{ncomponents}-comp"
+    if correct_extinction:
+        df_fname += "_extcorr"
+    df_fname += f"_minSNR={eline_SNR_min}"
+    if debug:
+        df_fname += "_DEBUG"
+    df_fname += ".hd5"
+
+    assert os.path.exists(os.path.join(sami_data_path, df_fname)),\
+        f"File {os.path.join(sami_data_path, df_fname)} does does not exist!"
+    
+    #######################################################################
+    # LOAD THE DATAFRAME FROM MEMORY
+    #######################################################################
+    df = pd.read_hdf(os.path.join(sami_data_path, df_fname))
+
+    return df
+
+###############################################################################
+def merge_datacubes(gal=None, plotit=False):
+    """
+    Merge the results from my own LZIFU with the SAMI component maps to 
+    determine the optimal number of components in each spaxel.
+
+    INPUTS
+    ---------------------------------------------------------------------------
+    gal:    int
+        GAMA ID of the galaxy.
+
+    plotit: bool
+        If True, show diagnostic plots. Useful for debugging.
+
+    OUTPUTS
+    ---------------------------------------------------------------------------
+    Nothing.
+
     """
     if gal is None:
-        gals = [int(f.split("_merge_comp.fits")[0]) for f in os.listdir(lzifu_data_path) if f.endswith("merge_comp.fits") and not f.startswith("._")]
+        gals = [int(f.split("_merge_comp.fits")[0]) for f in os.listdir(lzifu_products_path) if f.endswith("merge_comp.fits") and not f.startswith("._")]
     else:
         if type(gal) == list:
             gals = gal
@@ -138,7 +162,7 @@ def merge_datacubes(gal=None, plotit=False):
             gals = [gal]
         for gal in gals:
             assert type(gal) == int, "gal must be an integer!"
-            fname = os.path.join(lzifu_data_path, f"{gal}_merge_comp.fits")
+            fname = os.path.join(lzifu_products_path, f"{gal}_merge_comp.fits")
             assert os.path.exists(fname), f"File {fname} not found!"
 
     for gal in tqdm(gals):
@@ -147,7 +171,7 @@ def merge_datacubes(gal=None, plotit=False):
         # should be in each spaxel.
         ###############################################################################
         # Open the SAMI data.
-        hdulist_sami = fits.open(os.path.join(sami_data_path, f"ifs/{gal}/{gal}_A_Halpha_default_recom-comp.fits"))
+        hdulist_sami = fits.open(os.path.join(sami_data_path, f"{gal}/{gal}_A_Halpha_default_recom-comp.fits"))
         halpha_map = np.copy(hdulist_sami[0].data[1:])
 
         # Figure out how many components there are in each spaxel.
@@ -163,10 +187,10 @@ def merge_datacubes(gal=None, plotit=False):
         ###############################################################################
 
         # Open the FITS file saved in merge.pro.
-        hdulist_merged = fits.open(os.path.join(lzifu_data_path, f"{gal}_merge_comp.fits"))
-        hdulist_1 = fits.open(os.path.join(lzifu_data_path, f"{gal}_1_comp.fits"))
-        hdulist_2 = fits.open(os.path.join(lzifu_data_path, f"{gal}_2_comp.fits"))
-        hdulist_3 = fits.open(os.path.join(lzifu_data_path, f"{gal}_3_comp.fits"))
+        hdulist_merged = fits.open(os.path.join(lzifu_products_path, f"{gal}_merge_comp.fits"))
+        hdulist_1 = fits.open(os.path.join(lzifu_products_path, f"{gal}_1_comp.fits"))
+        hdulist_2 = fits.open(os.path.join(lzifu_products_path, f"{gal}_2_comp.fits"))
+        hdulist_3 = fits.open(os.path.join(lzifu_products_path, f"{gal}_3_comp.fits"))
 
         # For checking
         halpha_map_old = np.copy(hdulist_merged["HALPHA"].data[1:])
@@ -221,7 +245,7 @@ def merge_datacubes(gal=None, plotit=False):
         hdulist_merged[0].header["NOTE"] = "Number of components determined from SAMI DR3 data"
 
         # Save
-        hdulist_merged.writeto(os.path.join(lzifu_data_path, f"{gal}_merge_lzcomp.fits"), overwrite=True, output_verify="ignore")
+        hdulist_merged.writeto(os.path.join(lzifu_products_path, f"{gal}_merge_lzcomp.fits"), overwrite=True, output_verify="ignore")
 
         ###############################################################################
         # If desired, plot the Halpha maps in each component for both the 
@@ -293,30 +317,23 @@ if __name__ == "__main__":
     vgrad_cut = False
 
     df = load_lzifu_galaxies(gal=gal,
-                            ncomponents="recom", bin_type="default",
-                            eline_SNR_min=eline_SNR_min, vgrad_cut=vgrad_cut)
-    df_nocut = load_lzifu_galaxies(gal=gal,
-                            ncomponents="recom", bin_type="default",
-                            eline_SNR_min=0, vgrad_cut=False)
+                            ncomponents="recom", bin_type="default")
 
     # CHECK: proper S/N cuts have been made
     for eline in ["HALPHA", "HBETA", "OIII5007", "OI6300", "NII6583", "SII6716", "SII6731"]:
         plt.figure()
-        plt.scatter(x=df_nocut[f"{eline} (total)"], y=df_nocut[f"{eline} S/N (total)"], c="grey", alpha=0.1, s=5)
         plt.scatter(x=df[f"{eline} (total)"], y=df[f"{eline} S/N (total)"], c="k", s=5)
         plt.ylabel(f"{eline} (total) S/N")
         plt.axhline(eline_SNR_min, color="r")
         plt.xlabel(f"{eline} (total) flux")
     for ii in range(3):
         plt.figure()
-        plt.scatter(x=df_nocut[f"HALPHA (component {ii})"], y=df_nocut[f"HALPHA S/N (component {ii})"], c="grey", alpha=0.1, s=5)
         plt.scatter(x=df[f"HALPHA (component {ii})"], y=df[f"HALPHA S/N (component {ii})"], c="k", s=5)
         plt.ylabel(f"HALPHA (component {ii}) S/N")
         plt.axhline(eline_SNR_min, color="r")
         plt.xlabel(f"HALPHA (component {ii}) flux")
     for ii in range(3):
         plt.figure()
-        plt.scatter(x=df_nocut[f"HALPHA EW (component {ii})"], y=df_nocut[f"HALPHA S/N (component {ii})"], c="grey", alpha=0.1, s=5)
         plt.scatter(x=df[f"HALPHA EW (component {ii})"], y=df[f"HALPHA S/N (component {ii})"], c="k", s=5)
         plt.ylabel(f"HALPHA (component {ii}) S/N")
         plt.axhline(eline_SNR_min, color="r")
@@ -325,7 +342,6 @@ if __name__ == "__main__":
     # CHECK: gas kinematics
     for ii in range(3):
         plt.figure()
-        plt.scatter(x=df_nocut[f"sigma_obs S/N (component {ii})"], y=df_nocut[f"sigma_obs target S/N (component {ii})"], c="grey", alpha=0.1, s=5)
         plt.scatter(x=df[f"sigma_obs S/N (component {ii})"], y=df[f"sigma_obs target S/N (component {ii})"], c="k", s=5)
         plt.ylabel(f"sigma_obs target S/N (component {ii})")
         plt.axhline(3, color="b")
