0d340de438d581e7619c3ba1336194b76dddc80e
Henry Zovaro <henry.zovaro@anu.edu.au>
2022-07-11 14:53:36 +1000

    Last commit before moving over stuff from make_df_s7.py and make_s7_metadata_df.py

--- loaddata/s7.py
+++ loaddata/s7.py
@@ -2,13 +2,16 @@ import os
 import pandas as pd
 import numpy as np
 from scipy import constants
+from cosmocalc import get_dist
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 from spaxelsleuth.loaddata import dqcut, linefns
 
 from IPython.core.debugger import Tracer
 
 """
-This script contains the function load_sami_galaxies() which is used to 
+This script contains the function load_sami_df() which is used to 
 load the Pandas DataFrame containing spaxel-by-spaxel information for all 
 S7 galaxies that was created in make_df_s7.py.
 
@@ -27,6 +30,120 @@ or S/N requirements either droppped or replaced with NaNs.
 # Paths
 s7_data_path = os.environ["S7_DIR"]
 assert "S7_DIR" in os.environ, "Environment variable S7_DIR is not defined!"
+
+###############################################################################
+def make_s7_metadata_df():
+    """
+    DESCRIPTION
+    ---------------------------------------------------------------------------
+    This function is used to create a DataFrame containing "metadata", including
+    stellar masses, spectroscopic redshifts, morphologies and other information
+    for each galaxy in S7. In addition to the provided values in the input
+    catalogues, the angular scale (in kpc per arcsecond) and inclination are 
+    computed for each galaxy.
+
+    This script must be run before make_s7_df() as the resulting DataFrame
+    is used there.
+
+    The information used here is from the catalogues are available at 
+    https://datacentral.org.au/. 
+
+    USAGE
+    ---------------------------------------------------------------------------
+            
+            >>> from spaxelsleuth.loaddata.s7 import make_s7_metadata_df
+            >>> make_s7_metadata_df()
+
+    INPUTS
+    ---------------------------------------------------------------------------
+    None.
+
+    OUTPUTS
+    ---------------------------------------------------------------------------
+    The DataFrame is saved to 
+
+        S7_DIR/s7_metadata.hd5
+
+    PREREQUISITES
+    ---------------------------------------------------------------------------
+    S7_DIR must be defined as an environment variable.
+
+    The table containing metadata for S7 galaxies is required for this script. 
+    This has been included in the ../data/ directory but can be downloaded in 
+    CSV format from 
+        
+        https://miocene.anu.edu.au/S7/Data_release_2/S7_DR2_Table_2_Catalogue.csv
+    
+    """
+
+    ###############################################################################
+    # Filenames
+    df_metadata_fname = "S7_DR2_Table_2_Catalogue.csv"
+    df_fname = "s7_metadata.hd5"
+
+    ###############################################################################
+    # READ IN THE METADATA
+    ###############################################################################
+    df_metadata = pd.read_csv(os.path.join("../data", df_metadata_fname), skiprows=58)
+    gals = df_metadata["S7_Name"].values
+
+    ###############################################################################
+    # Convert object coordinates to degrees
+    ###############################################################################
+    coords = SkyCoord(df_metadata["RA_hms"], df_metadata["Dec_sxgsml"],
+                 unit=(u.hourangle, u.deg))
+    df_metadata["ra_obj"] = coords.ra.deg
+    df_metadata["dec_obj"] = coords.dec.deg
+
+    ###############################################################################
+    # Rename columns
+    ###############################################################################
+    rename_dict = {
+        "S7_Name": "catid",
+        "HL_inclination": "Inclination i (degrees)",
+        "HL_Re": "R_e (arcsec)",
+        "HL_Re_err": "R_e error (arcsec)",
+        "NED_ax_ratio": "b/a",
+        "NED_ax_ratio_err": "b/a error",
+        "HL_PA": "pa",
+        "S7_best_WiFeS_PA": "WiFeS PA",
+        "S7_Mstar": "mstar",
+        "S7_Mstar_err": "mstar error",
+        "S7_Sy1_subtraction?": "Sy1 subtraction?",
+        "S7_mosaic?": "Mosaic?",
+        "S7_BPT_classification": "BPT (global)",
+        "S7_z": "z_spec",
+        "S7_nucleus_index_x": "x0 (pixels)",
+        "S7_nucleus_index_y": "y0 (pixels)"
+    }
+    df_metadata = df_metadata.rename(columns=rename_dict)
+    df_metadata = df_metadata.set_index(df_metadata["catid"])
+
+    # Get rid of unneeded columns
+    good_cols = [rename_dict[k] for k in rename_dict.keys()] + ["ra_obj", "dec_obj"]
+    df_metadata = df_metadata[good_cols]
+
+    ###############################################################################
+    # Add angular scale info
+    ###############################################################################
+    for gal in gals:
+        D_A_Mpc, D_L_Mpc = get_dist(z=df_metadata.loc[gal, "z_spec"])
+        df_metadata.loc[gal, "D_A (Mpc)"] = D_A_Mpc
+        df_metadata.loc[gal, "D_L (Mpc)"] = D_L_Mpc
+    df_metadata["kpc per arcsec"] = df_metadata["D_A (Mpc)"] * 1e3 * np.pi / 180.0 / 3600.0
+
+    ###############################################################################
+    # Define a "Good?" column
+    ###############################################################################
+    df_metadata["Sy1 subtraction?"] = [True if x == "Y" else False for x in df_metadata["Sy1 subtraction?"].values]
+    df_metadata["Good?"] = ~df_metadata["Sy1 subtraction?"].values
+
+    ###############################################################################
+    # Save to file
+    ###############################################################################
+    df_metadata.to_hdf(os.path.join(s7_data_path, df_fname), key="metadata")
+
+    return
 
 ###############################################################################
 def load_s7_galaxies(eline_SNR_min, eline_list=["HALPHA", "HBETA", "NII6583", "OI6300", "OII3726", "OII3729", "OIII5007", "SII6716", "SII6731"],
