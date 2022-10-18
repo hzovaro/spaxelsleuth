26e36355697f174f95604706b2d7f76be7beda58
Henry Zovaro <henry.zovaro@anu.edu.au>
2022-10-19 10:19:07 +1100

    Removed make_sami_metadata_df_extended() - moved all the computations it contained to make_sami_metadata_df()

--- loaddata/sami.py
+++ loaddata/sami.py
@@ -38,12 +38,6 @@ This script contains the following functions:
         load a DataFrame containing emission line fluxes, etc. that was created 
         using make_sami_df().
 
-    make_sami_metadata_df_extended():
-        Make an "extended" version of the metadata DataFrame created using
-        make_sami_metadata_df() that includes continuum S/N measurements and
-        other useful quantities for selecting subsamples of the full SAMI
-        survey.        
-
 PREREQUISITES
 ------------------------------------------------------------------------------
 SAMI_DIR and SAMI_DATACUBE_DIR must be defined as an environment variable.
@@ -85,7 +79,75 @@ sami_datacube_path = os.environ["SAMI_DATACUBE_DIR"]
 assert "SAMI_DATACUBE_DIR" in os.environ, "Environment variable SAMI_DATACUBE_DIR is not defined!"
 
 ###############################################################################
-def make_sami_metadata_df():
+# For computing median continuum S/N values in make_sami_metadata_df()
+def _compute_snr(args, plotit=False):
+    gal, df_metadata = args
+
+    # Load the red & blue data cubes.
+    hdulist_R_cube = fits.open(os.path.join(sami_datacube_path, f"ifs/{gal}/{gal}_A_cube_red.fits.gz"))
+    hdulist_B_cube = fits.open(os.path.join(sami_datacube_path, f"ifs/{gal}/{gal}_A_cube_blue.fits.gz"))
+    data_cube_B = hdulist_B_cube[0].data
+    var_cube_B = hdulist_B_cube[1].data
+    data_cube_R = hdulist_R_cube[0].data
+    var_cube_R = hdulist_R_cube[1].data
+    hdulist_R_cube.close()
+    hdulist_B_cube.close()
+
+    # Compute an image showing the median S/N in each spaxel.
+    im_SNR_B = np.nanmedian(data_cube_B / np.sqrt(var_cube_B), axis=0)
+    im_SNR_R = np.nanmedian(data_cube_R / np.sqrt(var_cube_R), axis=0)
+
+    #######################################################################
+    # Use R_e to compute the median S/N within 1, 1.5, 2 R_e. 
+    # Transform coordinates into the galaxy plane
+    e = df_metadata.loc[gal, "ellip"]
+    PA = df_metadata.loc[gal, "pa"]
+    beta_rad = np.deg2rad(PA - 90)
+    b_over_a = 1 - e
+    q0 = 0.2
+    i_rad = np.arccos(np.sqrt((b_over_a**2 - q0**2) / (1 - q0**2)))  # Want to store this!
+    i_rad = 0 if np.isnan(i_rad) else i_rad
+
+    # De-project the centroids to the coordinate system of the galaxy plane
+    x0_px = 25.5
+    y0_px = 25.5
+    as_per_px = 0.5
+    ys, xs = np.meshgrid(np.arange(50), np.arange(50), indexing="ij")
+    x_cc = xs - x0_px  # pixels
+    y_cc = ys - y0_px  # pixels
+    x_prime = x_cc * np.cos(beta_rad) + y_cc * np.sin(beta_rad)
+    y_prime_projec = (- x_cc * np.sin(beta_rad) + y_cc * np.cos(beta_rad))
+    y_prime = (- x_cc * np.sin(beta_rad) + y_cc * np.cos(beta_rad)) / np.cos(i_rad)
+    r_prime = np.sqrt(x_prime**2 + y_prime**2)
+
+    # Convert to arcsec
+    r_prime_as = r_prime * as_per_px
+
+    # Masks enclosing differen multiples of R_e 
+    mask_1Re = r_prime_as < df_metadata.loc[gal, "remge"]
+    mask_15Re = r_prime_as < 1.5 * df_metadata.loc[gal, "remge"]
+    mask_2Re = r_prime_as < 2 * df_metadata.loc[gal, "remge"]
+
+    # Compute median SNR within 1, 1.5, 2R_e
+    SNR_full_B = np.nanmedian(im_SNR_B)
+    SNR_full_R = np.nanmedian(im_SNR_R)
+    SNR_1Re_B = np.nanmedian(im_SNR_B[mask_1Re])
+    SNR_1Re_R = np.nanmedian(im_SNR_R[mask_1Re])
+    SNR_15Re_B = np.nanmedian(im_SNR_B[mask_15Re])
+    SNR_15Re_R = np.nanmedian(im_SNR_R[mask_15Re])
+    SNR_2Re_B = np.nanmedian(im_SNR_B[mask_2Re])
+    SNR_2Re_R = np.nanmedian(im_SNR_R[mask_2Re])
+
+    #######################################################################
+    # End
+    print(f"In make_sami_metadata_df(): Finished processing {gal}")
+    return [gal, SNR_full_B, SNR_full_R, 
+                 SNR_1Re_B, SNR_1Re_R, 
+                 SNR_15Re_B, SNR_15Re_R, 
+                 SNR_2Re_B, SNR_2Re_R]
+
+###############################################################################
+def make_sami_metadata_df(recompute_continuum_SNRs=False, nthreads=20):
     """
     DESCRIPTION
     ---------------------------------------------------------------------------
@@ -109,13 +171,21 @@ def make_sami_metadata_df():
 
     INPUTS
     ---------------------------------------------------------------------------
-    None.
+    recompute_continuum_SNRs:   bool
+        If True, compute median continuum S/N values 
+
+    nthreads:                   int
+        Number of threads used to parallelise the continuum S/N values.
 
     OUTPUTS
     ---------------------------------------------------------------------------
     The DataFrame is saved to 
 
         SAMI_DIR/sami_dr3_metadata.hd5
+
+    The DataFrame containing continuum S/N values is saved to 
+
+        SAMI_DIR/sami_dr3_aperture_snrs.hd5
 
     PREREQUISITES
     ---------------------------------------------------------------------------
@@ -141,6 +211,7 @@ def make_sami_metadata_df():
                             - InputCatClustersDR3
                             - InputCatFiller
                             - VisualMorphologyDR3
+                            - MGEPhotomUnregDR3
 
      and stored at ../data/ using the naming convention
 
@@ -148,7 +219,8 @@ def make_sami_metadata_df():
         sami_InputCatClustersDR3.csv
         sami_InputCatFiller.csv
         sami_VisualMorphologyDR3.csv
-        sami_CubeObs.csv.
+        sami_CubeObs.csv
+        sami_MGEPhotomUnregDR3.csv.
 
     """
     print("In make_sami_metadata_df(): Creating metadata DataFrame...")
@@ -296,6 +368,48 @@ def make_sami_metadata_df():
     q0 = 0.2
     i_rad = np.arccos(np.sqrt((b_over_a**2 - q0**2) / (1 - q0**2)))  # Want to store this!
     df_metadata["Inclination i (degrees)"] = np.rad2deg(i_rad)
+
+    ###############################################################################
+    # Compute continuum SNRs from the data cubes
+    ###############################################################################
+    print("In make_sami_metadata_df(): Computing continuum SNRs...")
+    if not recompute_continuum_SNRs and os.path.exists(os.path.join(sami_data_path, "sami_dr3_aperture_snrs.hd5")):
+        print(f"In make_sami_metadata_df(): WARNING: file {os.path.join(sami_data_path, 'sami_dr3_aperture_snrs.hd5')} found; loading SNRs from existing DataFrame...")
+        df_snr = pd.read_hdf(os.path.join(sami_data_path, "sami_dr3_aperture_snrs.hd5"), key="SNR")
+    else:
+        print(f"In make_sami_metadata_df(): WARNING: computing continuum SNRs on {nthreads} threads...")
+        print("In make_sami_metadata_df(): Beginning pool...")
+        args_list = [[gal, df_metadata] for gal in gals]
+        pool = multiprocessing.Pool(nthreads)
+        res_list = np.array((pool.map(_compute_snr, args_list)))
+        pool.close()
+        pool.join()
+
+        ###########################################################################
+        # Create DataFrame from results
+        ###############################################################################
+        df_snr = pd.DataFrame(np.vstack(res_list), columns=["catid",
+                                                            "Median SNR (B, full field)",
+                                                            "Median SNR (R, full field)",
+                                                            "Median SNR (B, 1R_e)",
+                                                            "Median SNR (R, 1R_e)",
+                                                            "Median SNR (B, 1.5R_e)",
+                                                            "Median SNR (R, 1.5R_e)",
+                                                            "Median SNR (B, 2R_e)",
+                                                            "Median SNR (R, 2R_e)"])
+        df_snr["catid"] = df_snr["catid"].astype(int)
+        df_snr.set_index("catid")
+
+        # Save 
+        print("In make_sami_metadata_df(): Saving aperture SNR DataFrame to file...")
+        df_snr.to_hdf(os.path.join(sami_data_path, "sami_dr3_aperture_snrs.hd5"), key="SNR")
+
+    ###############################################################################
+    # Merge with the metadata DataFrame
+    ###############################################################################
+    common_cols = [c for c in df_metadata.columns if c not in df_snr.columns]
+    df_snr = df_snr.set_index("catid")
+    df_snr = pd.concat([df_snr, df_metadata], axis=1)
 
     ###########################################################################
     # Save to file
@@ -1327,184 +1441,6 @@ def load_sami_df(ncomponents, bin_type, correct_extinction, eline_SNR_min,
     # Return
     print("In load_sami_df(): Finished!")
     return df.sort_index()
-
-###############################################################################
-# For multithreading
-def _compute_snr(args, plotit=False):
-    gal, df_metadata = args
-
-    # Load the red & blue data cubes.
-    hdulist_R_cube = fits.open(os.path.join(sami_datacube_path, f"ifs/{gal}/{gal}_A_cube_red.fits.gz"))
-    hdulist_B_cube = fits.open(os.path.join(sami_datacube_path, f"ifs/{gal}/{gal}_A_cube_blue.fits.gz"))
-    data_cube_B = hdulist_B_cube[0].data
-    var_cube_B = hdulist_B_cube[1].data
-    data_cube_R = hdulist_R_cube[0].data
-    var_cube_R = hdulist_R_cube[1].data
-    hdulist_R_cube.close()
-    hdulist_B_cube.close()
-
-    # Compute an image showing the median S/N in each spaxel.
-    im_SNR_B = np.nanmedian(data_cube_B / np.sqrt(var_cube_B), axis=0)
-    im_SNR_R = np.nanmedian(data_cube_R / np.sqrt(var_cube_R), axis=0)
-
-    #######################################################################
-    # Use R_e to compute the median S/N within 1, 1.5, 2 R_e. 
-    # Transform coordinates into the galaxy plane
-    e = df_metadata.loc[gal, "ellip"]
-    PA = df_metadata.loc[gal, "pa"]
-    beta_rad = np.deg2rad(PA - 90)
-    b_over_a = 1 - e
-    q0 = 0.2
-    i_rad = np.arccos(np.sqrt((b_over_a**2 - q0**2) / (1 - q0**2)))  # Want to store this!
-    i_rad = 0 if np.isnan(i_rad) else i_rad
-
-    # De-project the centroids to the coordinate system of the galaxy plane
-    x0_px = 25.5
-    y0_px = 25.5
-    as_per_px = 0.5
-    ys, xs = np.meshgrid(np.arange(50), np.arange(50), indexing="ij")
-    x_cc = xs - x0_px  # pixels
-    y_cc = ys - y0_px  # pixels
-    x_prime = x_cc * np.cos(beta_rad) + y_cc * np.sin(beta_rad)
-    y_prime_projec = (- x_cc * np.sin(beta_rad) + y_cc * np.cos(beta_rad))
-    y_prime = (- x_cc * np.sin(beta_rad) + y_cc * np.cos(beta_rad)) / np.cos(i_rad)
-    r_prime = np.sqrt(x_prime**2 + y_prime**2)
-
-    # Convert to arcsec
-    r_prime_as = r_prime * as_per_px
-
-    # Masks enclosing differen multiples of R_e 
-    mask_1Re = r_prime_as < df_metadata.loc[gal, "remge"]
-    mask_15Re = r_prime_as < 1.5 * df_metadata.loc[gal, "remge"]
-    mask_2Re = r_prime_as < 2 * df_metadata.loc[gal, "remge"]
-
-    # Compute median SNR within 1, 1.5, 2R_e
-    SNR_full_B = np.nanmedian(im_SNR_B)
-    SNR_full_R = np.nanmedian(im_SNR_R)
-    SNR_1Re_B = np.nanmedian(im_SNR_B[mask_1Re])
-    SNR_1Re_R = np.nanmedian(im_SNR_R[mask_1Re])
-    SNR_15Re_B = np.nanmedian(im_SNR_B[mask_15Re])
-    SNR_15Re_R = np.nanmedian(im_SNR_R[mask_15Re])
-    SNR_2Re_B = np.nanmedian(im_SNR_B[mask_2Re])
-    SNR_2Re_R = np.nanmedian(im_SNR_R[mask_2Re])
-
-    #######################################################################
-    # End
-    print(f"In make_sami_metadata_df_extended(): Finished processing {gal}")
-    return [gal, SNR_full_B, SNR_full_R, 
-                 SNR_1Re_B, SNR_1Re_R, 
-                 SNR_15Re_B, SNR_15Re_R, 
-                 SNR_2Re_B, SNR_2Re_R]
-
-###############################################################################
-def make_sami_metadata_df_extended(recompute_SNRs=False, nthreads=20):
-    """
-    DESCRIPTION
-    ---------------------------------------------------------------------------
-    This function is used to create an extended version of the metadata 
-    DataFrame created using make_sami_metadata_df().
-
-    Additional quantities computed include 
-    - compute continuum S/N values in both blue & red SAMI cubes in a variety of 
-      different apertures, making it simpler to select high-S/N targets for your
-      science case;
-    - the maximum number of components fitted in any spaxel in each galaxy.
-
-    The extended metadata DataFrame is saved to 
-    SAMI_DR/sami_dr3_metadata_extended.hd5.
-
-    When run the first time, an additional DataFrame is created ("aperture_snrs.hd5")
-    to store the SNR measurements. Additional runs of this script will load 
-    this DataFrame if it exists rather than re-computing the SNRs.
-
-    USAGE
-    ---------------------------------------------------------------------------
-            
-            >>> from spaxelsleuth.loaddata.sami import make_sami_metadata_df_extended
-            >>> make_sami_metadata_df_extended()
-
-    INPUTS
-    ---------------------------------------------------------------------------
-    recompute_SNRs      bool
-        if True, recompute continuum signal-to-noise ratios. If False, the 
-        computation is only run if the file sami_dr3_aperture_snrs.hd5 is not 
-        found.
-
-    OUTPUTS
-    ---------------------------------------------------------------------------
-    The DataFrame is saved to 
-
-        SAMI_DIR/sami_dr3_metadata_extended.hd5
-
-    PREREQUISITES
-    ---------------------------------------------------------------------------
-    Both make_sami_metadata_df() and make_sami_df() must be run first, as this 
-    script requires both the metadata DataFrame (located at 
-    SAMI_DIR/sami_dr3_metadata.hd5) and the DataFrame containing spaxel-by-
-    spaxel information for every SAMI galaxy.
-
-    """
-    print("In make_sami_metadata_df_extended(): Creating extended metadata DataFrame...")
-    ###############################################################################
-    # Load the metadata
-    assert os.path.exists(os.path.join(sami_data_path, "sami_dr3_metadata.hd5")),\
-        f"File {os.path.join(sami_data_path, 'sami_dr3_metadata.hd5')} not found!"
-    df_metadata = pd.read_hdf(os.path.join(sami_data_path, "sami_dr3_metadata.hd5"), key="metadata")
-    df_fname = "sami_dr3_metadata_extended.hd5"
-
-    # Obtain list of galaxies
-    gals = df_metadata[df_metadata["Good?"] == True].index.values
-
-    ###############################################################################
-    # Compute SNRs
-    ###############################################################################
-    print("In make_sami_metadata_df_extended(): Computing continuum SNRs...")
-    if not recompute_SNRs and os.path.exists(os.path.join(sami_data_path, "sami_dr3_aperture_snrs.hd5")):
-        print(f"In make_sami_metadata_df_extended(): WARNING: file {os.path.join(sami_data_path, 'sami_dr3_aperture_snrs.hd5')} found; loading SNRs from existing DataFrame...")
-        df_snr = pd.read_hdf(os.path.join(sami_data_path, "sami_dr3_aperture_snrs.hd5"), key="SNR")
-    else:
-        print(f"In make_sami_metadata_df_extended(): WARNING: file {os.path.join(sami_data_path, 'sami_dr3_aperture_snrs.hd5')} not found; computing continuum SNRs on {nthreads} threads...")
-        print("In make_sami_metadata_df_extended(): Beginning pool...")
-        args_list = [[gal, df_metadata] for gal in gals]
-        pool = multiprocessing.Pool(nthreads)
-        res_list = np.array((pool.map(_compute_snr, args_list)))
-        pool.close()
-        pool.join()
-
-        ###########################################################################
-        # Create DataFrame from results
-        ###############################################################################
-        df_snr = pd.DataFrame(np.vstack(res_list), columns=["catid",
-                                                            "Median SNR (B, full field)",
-                                                            "Median SNR (R, full field)",
-                                                            "Median SNR (B, 1R_e)",
-                                                            "Median SNR (R, 1R_e)",
-                                                            "Median SNR (B, 1.5R_e)",
-                                                            "Median SNR (R, 1.5R_e)",
-                                                            "Median SNR (B, 2R_e)",
-                                                            "Median SNR (R, 2R_e)"])
-        df_snr["catid"] = df_snr["catid"].astype(int)
-        df_snr.set_index("catid")
-
-        # Save 
-        print("In make_sami_metadata_df_extended(): Saving aperture SNR DataFrame to file...")
-        df_snr.to_hdf(os.path.join(sami_data_path, "sami_dr3_aperture_snrs.hd5"), key="SNR")
-
-    ###############################################################################
-    # Merge with the metadata DataFrame
-    ###############################################################################
-    common_cols = [c for c in df_metadata.columns if c not in df_snr.columns]
-    df_snr = df_snr.set_index("catid")
-    df_snr = pd.concat([df_snr, df_metadata], axis=1)
-
-    ###############################################################################
-    # Save to .csv 
-    ###############################################################################
-    print(f"In make_sami_metadata_df_extended(): Saving extnded metadata DataFrame to file {os.path.join(sami_data_path, df_fname)}...")
-    df_snr.to_hdf(os.path.join(sami_data_path, df_fname), key="Extended metadata")
-
-    print(f"In make_sami_metadata_df_extended(): Finished!")
-    return 
 
 ###############################################################################
 def make_sami_aperture_df(eline_SNR_min, 
