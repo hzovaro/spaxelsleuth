1982514b2065a1f288f5fa66d750ebd31453d96b
Henry Zovaro <henry.zovaro@anu.edu.au>
2022-12-09 14:48:46 +1100

    Replaced extinction_corr_fn() with two separate functions: one to compute A_V and another to apply it in a separate step

--- utils/extcorr.py
+++ utils/extcorr.py
@@ -65,13 +65,13 @@ def extcorr_helper_fn(args):
     Function used to parallelise extinction correction computation in
     extinction_corr_fn().
     """
-    rr, df, eline_list, ext_fn = args 
+    rr, df, eline_list, a_v_col_name, ext_fn = args 
     df_row = df.loc[rr]
-    if df_row[f"A_V"] > 0:
+    if df_row[a_v_col_name] > 0:
         for eline in eline_list:
             lambda_A = eline_lambdas_A[eline]
             A_line = ext_fn(wave=np.array([lambda_A]), 
-                            a_v=df_row[f"A_V"], 
+                            a_v=df_row[a_v_col_name], 
                             unit="aa")[0]
             
             # Apply correction
@@ -81,19 +81,19 @@ def extcorr_helper_fn(args):
     return df_row
 
 ################################################################################
-def extinction_corr_fn(df, eline_list,
-                       reddening_curve="fm07", R_V=3.1, 
-                       balmer_SNR_min=5,
-                       nthreads=20,
-                       s=None):
-    
+def compute_A_V(df, 
+                reddening_curve="fm07", R_V=3.1, 
+                balmer_SNR_min=5,
+                s=None):
     """
-    Correct emission line fluxes (and errors) for extinction.
+    Compute extinction in the V-band (A_V) using the Balmer decrement.
 
     INPUTS
     ------------------------------------------------------------------------
-    eline_list:         list of str
-        Emission line fluxes to correct.
+    df:                 Pandas DataFrame
+        DataFrame containing emission line fluxes. Must contain columns
+        "HALPHA" and "HBETA" where the column names may have a suffix 
+        defined by s.
 
     reddening_curve:    str
         Reddening curve to assume. Defaults to Fitzpatrick & Massa (2007).
@@ -106,14 +106,9 @@ def extinction_corr_fn(df, eline_list,
         Minimum SNR of the HALPHA and HBETA fluxes to accept in order to compute 
         A_V.
 
-    nthreads:           int
-        Number of threads on which to concurrently compute extinction correction
-        factors. 
 
     RETURNS
     ------------------------------------------------------------------------
-    The input DataFrame with new columns added containing the Balmer decrement,
-    A_V, and corresponding errors.
 
     """
     #//////////////////////////////////////////////////////////////////////////
@@ -154,7 +149,6 @@ def extinction_corr_fn(df, eline_list,
     E_ba_err = 2.5 / np.log(10) * df[f"Balmer decrement error"] / df[f"Balmer decrement"]
 
     # Calculate ( A(Ha) - A(Hb) ) / E(B-V) from extinction curve
-    R_V = 3.1
     wave_1_A = np.array([eline_lambdas_A["HALPHA"]])
     wave_2_A = np.array([eline_lambdas_A["HBETA"]])
     E_ba_over_E_BV = float(ext_fn(wave_2_A, a_v=1.0) - ext_fn(wave_1_A, a_v=1.0) ) /  1.0 * R_V
@@ -181,13 +175,84 @@ def extinction_corr_fn(df, eline_list,
     df.loc[cond_bad_A_V, f"A_V"] = np.nan
     df.loc[cond_bad_A_V, f"A_V error"] = np.nan
 
+    #//////////////////////////////////////////////////////////////////////////
+    # Rename columns
+    #//////////////////////////////////////////////////////////////////////////
+    if s is not None:
+        # Get list of new columns that have been added
+        added_cols = [c for c in df.columns if c not in old_cols]
+        suffix_added_cols = [f"{c}{s}" for c in added_cols]
+        # Rename the new columns
+        df = df.rename(columns=dict(zip(added_cols, suffix_added_cols)))
+        # Replace the suffix in the column names
+        df = df.rename(columns=dict(zip(suffix_removed_cols, suffix_cols)))
+
+    return df
+
+################################################################################
+def apply_extinction_correction(df, eline_list, a_v_col_name, 
+                                reddening_curve="fm07",
+                                nthreads=20,
+                                s=None):
+    
+    """
+    Correct emission line fluxes (and errors) for extinction.
+
+    INPUTS
+    ------------------------------------------------------------------------
+    eline_list:         list of str
+        Emission line fluxes to correct.
+
+    a_v_col_name:       str
+        Column in the DataFrame containing A_V values, e.g. "A_V (total)". 
+
+        This is useful if you would like to e.g. apply the extinction correction
+        to components 1, 2 and 3 using the A_V calculated from the total 
+        emission line fluxes.
+
+    nthreads:           int
+        Number of threads on which to concurrently compute extinction correction
+        factors. 
+
+    RETURNS
+    ------------------------------------------------------------------------
+    The input DataFrame with new columns added containing the Balmer decrement,
+    A_V, and corresponding errors.
+
+    """
+    #//////////////////////////////////////////////////////////////////////////
+    # Remove suffixes on columns
+    #//////////////////////////////////////////////////////////////////////////
+    if s is not None:
+        df_old = df
+        suffix_cols = [c for c in df.columns if c.endswith(s) and c != a_v_col_name]
+        suffix_removed_cols = [c.split(s)[0] for c in suffix_cols]
+        df = df_old.rename(columns=dict(zip(suffix_cols, suffix_removed_cols)))
+    old_cols = df.columns
+
+    # Check that A_V is in the DataFrame
+    assert a_v_col_name in df.columns,\
+        f"Column containing A_V values {a_v_col_name} not found in DataFrame!"
+
+    # Determine which reddening curve to use
+    if reddening_curve.lower() == "fm07":
+        ext_fn = extinction.fm07
+    elif reddening_curve.lower() == "ccm89":
+        ext_fn = extinction.ccm89
+    elif reddening_curve.lower() == "calzetti00":
+        ext_fn = extinction.calzetti00
+    else:  
+        raise ValueError("For now, 'reddening_curve' must be one of 'fm07', 'ccm89' or 'calzetti00'!")
+
+    #//////////////////////////////////////////////////////////////////////////
+    # Correct emission line fluxes in cells where A_V > 0
+    #//////////////////////////////////////////////////////////////////////////
     # Split into 2 DataFrames
-    cond_extcorr = df["A_V"] > 0
+    cond_extcorr = df[a_v_col_name] > 0
     df_extcorr = df[cond_extcorr]
     df_noextcorr = df[~cond_extcorr]
-
-    # If there are no spaxels in which A_V can be computed, return
     if df_extcorr.shape[0] == 0:
+        print(f"In extcorr.extinction_corr_fn(): no cells found with A_V > 0 - not applying correction!")
         if s is not None:
             # Get list of new columns that have been added
             added_cols = [c for c in df.columns if c not in old_cols]
@@ -198,15 +263,12 @@ def extinction_corr_fn(df, eline_list,
             df = df.rename(columns=dict(zip(suffix_removed_cols, suffix_cols)))
         return df
 
-    #//////////////////////////////////////////////////////////////////////////
-    # Correct emission line fluxes in cells where A_V > 0
-    #//////////////////////////////////////////////////////////////////////////
     # Turn off "settingwithcopy" warning because it pops up in extcorr_helper_fn,
     # even though we ARE changing the values properly.
     pd.options.mode.chained_assignment = None
 
     # Multithreading 
-    args_list = [[rr, df_extcorr, eline_list, ext_fn] for rr in df_extcorr.index.values]
+    args_list = [[rr, df_extcorr, eline_list, a_v_col_name, ext_fn] for rr in df_extcorr.index.values]
     print(f"In extcorr.extinction_corr_fn(): Multithreading A_V computation across {nthreads} threads...")
     pool = multiprocessing.Pool(nthreads)
     res_list = pool.map(extcorr_helper_fn, args_list)
