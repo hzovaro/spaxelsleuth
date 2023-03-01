d4ff5f32c037b78c62458381ef724eeab72d5cc8
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-03-01 14:17:08 +1100

    Now raises an exception if any tests fail

--- tests/make_sami_test_dfs.py
+++ tests/make_sami_test_dfs.py
@@ -1,25 +1,29 @@
-from spaxelsleuth.loaddata.sami import make_sami_metadata_df, make_sami_df, make_sami_aperture_df
 
-###########################################################################
-# Create the metadata DataFrame
-###########################################################################
-make_sami_metadata_df()
+if __name__ == "__main__":
+    from spaxelsleuth.loaddata.sami import make_sami_metadata_df, make_sami_df, make_sami_aperture_df
+    try:
+        ###########################################################################
+        # Create the metadata DataFrame
+        ###########################################################################
+        make_sami_metadata_df()
 
-###########################################################################
-# Create the DataFrames
-###########################################################################
-# Make test data 
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True)
-make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True)
-make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True)
-make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True)
-make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True)
-make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True)
+        ###########################################################################
+        # Create the DataFrames
+        ###########################################################################
+        # Make test data 
+        make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True)
+        make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True)
+        make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True)
+        make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True)
+        make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True)
+        make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True)
 
-# w/o extinction correction
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=False, debug=True)
-make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=False, debug=True)
-make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=False, debug=True)
-make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=False, debug=True)
-make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=False, debug=True)
-make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=False, debug=True)
+        # w/o extinction correction
+        make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=False, debug=True)
+        make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=False, debug=True)
+        make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=False, debug=True)
+        make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=False, debug=True)
+        make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=False, debug=True)
+        make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=False, debug=True)
+    except:
+        print("Exception raised: aborting!")
