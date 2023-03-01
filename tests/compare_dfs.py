a1f7cae33d19193430a3e9dd879184a8e14acd26
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-03-01 16:37:24 +1100

    Comparison now works on floats

--- tests/compare_dfs.py
+++ tests/compare_dfs.py
@@ -1,9 +1,9 @@
 # Compare the old and new DataFrames to monitor for any changes 
 if __name__ == "__main__":
-    import sys 
-    from pathlib import Path
-
+    import numpy as np
     import pandas as pd
+    from pathlib import Path
+    import sys 
 
     fname = sys.argv[1]
     fname_new = Path("/priv/meggs3/u5708159/SAMI/sami_dr3") / fname
@@ -12,10 +12,24 @@ if __name__ == "__main__":
     df_new = pd.read_hdf(fname_new)
     df_old = pd.read_hdf(fname_old)
 
-    # Compare DataFrames
-    if df_old.equals(df_new):
-        print("Old and new DataFrames match!")
-        sys.exit(0)
-    else:
-        print("ERROR: old and new DataFrames do not match!")
-        sys.exit(1)
+    # Check that shapes are the same 
+    assert df_old.shape == df_new.shape
+
+    # Check that there are no columns or rows that are different 
+    assert len([c for c in df_old if c not in df_new]) == 0
+    assert len([c for c in df_new if c not in df_old]) == 0
+    assert all(df_old.index == df_new.index)
+
+    # Since the order of the columns might change, we need to check them one by one.
+    # We don't consider metallicity/ionisation parameter columns because these are computed in a non-deterministic way and won't agree between runs.
+    for c in [c for c in df_old if "log(O/H) + 12" not in c and "log(U)" not in c]:
+        
+        # Check that the NaNs agree
+        all(df_new[c].isna() == df_old[c].isna())
+        
+        # Check that values agree 
+        cond_not_nan = ~df_old[c].isna()
+        if df_old[c].dtype != "object":
+            assert np.allclose(df_old.loc[cond_not_nan, c], df_new.loc[cond_not_nan, c]) 
+        else:
+            assert df_old[c].equals(df_new[c])
