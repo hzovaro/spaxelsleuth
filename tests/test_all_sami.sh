257f91283c0866b7d980cac1c7e7442d06b06b66
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-03-01 15:27:33 +1100

    Added copmarison checks with old versions of DataFrames

--- tests/test_all_sami.sh
+++ tests/test_all_sami.sh
@@ -1,6 +1,20 @@
 # Make DataFrames 
 python make_sami_test_dfs.py
 
+# Compare with old verions
+python compare_dfs.py sami_default_recom-comp_extcorr_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_adaptive_recom-comp_extcorr_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_sectors_recom-comp_extcorr_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_default_1-comp_extcorr_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_adaptive_1-comp_extcorr_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_sectors_1-comp_extcorr_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_default_recom-comp_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_adaptive_recom-comp_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_sectors_recom-comp_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_default_1-comp_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_adaptive_1-comp_minSNR=5_DEBUG.hd5
+python compare_dfs.py sami_sectors_1-comp_minSNR=5_DEBUG.hd5
+
 # Run assertion tests 
 python test_assertions.py recom default 5
 python test_assertions.py 1 default 5
