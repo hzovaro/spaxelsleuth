528d631ba324758868bf97fc5a234bae5d686d1d
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-03-06 14:49:19 +1100

    Now loads custom config file to initialise paths

--- tests/make_sami_test_dfs.py
+++ tests/make_sami_test_dfs.py
@@ -1,29 +1,26 @@
 
 if __name__ == "__main__":
+    from itertools import product
+    import sys
+
+    from spaxelsleuth import load_user_config
+    load_user_config("/home/u5708159/.spaxelsleuthconfig.json")
     from spaxelsleuth.loaddata.sami import make_sami_metadata_df, make_sami_df, make_sami_aperture_df
-    try:
-        ###########################################################################
-        # Create the metadata DataFrame
-        ###########################################################################
-        make_sami_metadata_df()
 
-        ###########################################################################
-        # Create the DataFrames
-        ###########################################################################
-        # Make test data 
-        make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True)
-        make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True)
-        make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True)
-        make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True)
-        make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True)
-        make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True)
+    ###########################################################################
+    # Create the metadata DataFrame
+    ###########################################################################
+    make_sami_metadata_df()
 
-        # w/o extinction correction
-        make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=False, debug=True)
-        make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=False, debug=True)
-        make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=False, debug=True)
-        make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=False, debug=True)
-        make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=False, debug=True)
-        make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=False, debug=True)
-    except:
-        print("Exception raised: aborting!")
+    ###########################################################################
+    # Create the DataFrames
+    ###########################################################################
+    # Make test data 
+    for ncomponents, bin_type, correct_extinction in product(["recom", "1"], ["default", "adaptive", "sectors"], [True, False]):
+        try:
+            make_sami_df(ncomponents=ncomponents, bin_type=bin_type, eline_SNR_min=5, correct_extinction=correct_extinction, debug=True)
+        except:
+            print(f"ERROR: failed to make DataFrame with ncomponents={ncomponents}, bin_type={bin_type}, correct_extinction={correct_extinction}")
+            sys.exit(1)
+    sys.exit(0)
+    
