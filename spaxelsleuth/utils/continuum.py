e02b0b1593db75ab2056f7cc36b1566f07eddb31
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-08-17 14:11:29 +1000

    First commit

---
+++ spaxelsleuth/utils/continuum.py
@@ -0,0 +1,96 @@
+import numpy as np
+from scipy import constants
+
+from spaxelsleuth.utils.velocity import get_slices_in_velocity_range
+from spaxelsleuth.utils.misc import in_dataframe
+
+###############################################################################
+def compute_d4000(data_cube, var_cube, lambda_vals_rest_A, v_star_map):
+    """Compute the D4000Å break strength in a given data cube.
+    Definition from Balogh+1999 (see here: https://arxiv.org/pdf/1611.07050.pdf, page 3)"""
+
+    # Convert datacube & variance cubes to units of F_nu
+    data_cube_Hz = data_cube * lambda_vals_rest_A[:, None, None]**2 / (constants.c * 1e10)
+    var_cube_Hz2 = var_cube * (lambda_vals_rest_A[:, None, None]**2 / (constants.c * 1e10))**2
+
+    data_cube_b_masked, var_cube_b_masked = get_slices_in_velocity_range(data_cube_Hz, var_cube_Hz2, lambda_vals_rest_A, 3850, 3950, v_star_map)
+    data_cube_r_masked, var_cube_r_masked = get_slices_in_velocity_range(data_cube_Hz, var_cube_Hz2, lambda_vals_rest_A, 4000, 4100, v_star_map)
+
+    N_b = np.nansum(~np.isnan(data_cube_b_masked), axis=0)
+    N_r = np.nansum(~np.isnan(data_cube_r_masked), axis=0)
+
+    num = np.nanmean(data_cube_r_masked, axis=0)
+    denom = np.nanmean(data_cube_b_masked, axis=0)
+    err_num = 1 / N_r * np.sqrt(np.nansum(var_cube_r_masked, axis=0))
+    err_denom = 1 / N_b * np.sqrt(np.nansum(var_cube_b_masked, axis=0))
+
+    d4000_map = num / denom
+    d4000_map_err = d4000_map * np.sqrt((err_num / num)**2 + (err_denom / denom)**2)
+    return d4000_map, d4000_map_err
+
+###############################################################################
+def compute_continuum_intensity(data_cube, var_cube, lambda_vals_rest_A, start_A, stop_A, v_map):
+    """Compute the mean, std. dev. and error of the mean of the continuum between start_A and stop_A."""
+    data_cube_masked, var_cube_masked = get_slices_in_velocity_range(data_cube, var_cube, lambda_vals_rest_A, start_A, stop_A, v_map)
+    cont_map = np.nanmean(data_cube_masked, axis=0)
+    cont_map_std = np.nanstd(data_cube_masked, axis=0)
+    N = np.nansum(~np.isnan(data_cube_masked), axis=0)
+    cont_map_err = 1 / N * np.sqrt(np.nansum(var_cube_masked, axis=0))
+    # NOTE: N = 0 in the outskirts of the image, so dividing by 1 / N replaces these elements with NaN (which is what we want!)
+    return cont_map, cont_map_std, cont_map_err
+
+######################################################################
+def compute_continuum_luminosity(df):
+    """Compute HALPHA continuum luminosity."""
+    # HALPHA cont. luminosity: units of erg s^-1 Å-1 kpc^-2
+    if all([col in df for col in ["D_L (Mpc)", "Bin size (square kpc)"]]):
+        if all([col in df for col in ["HALPHA continuum", "HALPHA continuum error"]]):
+            df[f"HALPHA continuum luminosity"] = df[f"HALPHA continuum"] * 1e-16 * 4 * np.pi * (df["D_L (Mpc)"] * 1e6 * 3.086e18)**2 * 1 / df["Bin size (square kpc)"]
+            df[f"HALPHA continuum luminosity error"] = df[f"HALPHA continuum error"] * 1e-16 * 4 * np.pi * (df["D_L (Mpc)"] * 1e6 * 3.086e18)**2 * 1 / df["Bin size (square kpc)"]
+
+    return df
+
+###############################################################################
+def compute_EW(df, ncomponents_max, eline_list):
+    """Calculate equivalent widths (EWs) for the emission lines.
+        The EW is defined as 
+            
+            EW = emission line flux / continuum level in some wavelength range
+        
+        This function looks looks for the following columns in df:
+            <eline> (component <n>) -- emission line flux 
+            <eline> continuum -- corresponding continuum level
+
+    """
+    for eline in eline_list:
+        if in_dataframe(df, [f"{eline} continuum"]):
+            # Zero out -ve continuum values
+            df.loc[df[f"{eline} continuum"] < 0, f"{eline} continuum"] = 0
+
+            # Compute EW in each component
+            for nn in range(ncomponents_max):
+                if in_dataframe(df, f"{eline} (component {nn + 1})"):
+                    df[f"{eline} EW (component {nn + 1})"] = df[f"{eline} (component {nn + 1})"] / df[f"{eline} continuum"]
+                    # Compute associated errors
+                    if in_dataframe(df, [f"{eline} error (component {nn + 1})", f"{eline} continuum error"]):
+                        df[f"{eline} EW error (component {nn + 1})"] = df[f"{eline} EW (component {nn + 1})"] *\
+                            np.sqrt((df[f"{eline} error (component {nn + 1})"] / df[f"{eline} (component {nn + 1})"])**2 +\
+                                    (df[f"{eline} continuum error"] / df[f"{eline} continuum"])**2)
+
+                    # If the continuum level <= 0, then the EW is undefined, so set to NaN.
+                    df.loc[df[f"{eline} continuum"] <= 0, [f"{eline} EW (component {nn + 1})"]] = np.nan
+                    if in_dataframe(df, [f"{eline} EW error (component {nn + 1})"]):
+                        df.loc[df[f"{eline} continuum"] <= 0, [f"{eline} EW error (component {nn + 1})"]] = np.nan
+
+            # Calculate total EW
+            if in_dataframe(df, f"{eline} (total)"):
+                df[f"{eline} EW (total)"] = df[f"{eline} (total)"] / df[f"{eline} continuum"]
+                if in_dataframe(df, [f"{eline} error (total)", f"{eline} continuum error"]):
+                    df[f"{eline} EW error (total)"] = df[f"{eline} EW (total)"] *\
+                        np.sqrt((df[f"{eline} error (total)"] / df[f"{eline} (total)"])**2 +\
+                                (df[f"{eline} continuum error"] / df[f"{eline} continuum"])**2)
+
+                # If the continuum level <= 0, then the EW is undefined, so set to NaN.
+                df.loc[df[f"{eline} continuum"] <= 0, [f"{eline} EW (total)", f"{eline} EW error (total)"]] = np.nan
+
+    return df
---
+++ spaxelsleuth/utils/velocity.py
@@ -0,0 +1,59 @@
+from itertools import product
+import numpy as np
+from scipy import constants
+
+###############################################################################
+def get_wavelength_from_velocity(lambda_rest, v, units):
+    """Compute the Doppler-shifted wavelength given a velocity and a rest-frame wavelength."""
+    if units not in ['m/s', 'km/s',]:
+        raise ValueError("units must be m/s or km/s!")
+    if units == 'm/s':
+        v_m_s = v
+    elif units == 'km/s':
+        v_m_s = v * 1e3
+    lambda_obs = lambda_rest * np.sqrt((1 + v_m_s / constants.c) /
+                                       (1 - v_m_s / constants.c))
+    return lambda_obs
+
+
+###############################################################################
+def get_slices_in_velocity_range(data_cube, var_cube, lambda_vals_rest_A, lambda_rest_start_A, lambda_rest_stop_A, v_map):
+    """Returns a copy of the data/variance cubes with slices outside those in a specified wavelength range masked out."""
+    # 3D array containing wavelength values in each spaxel
+    lambda_vals_rest_A_cube = np.zeros(data_cube.shape)
+    lambda_vals_rest_A_cube[:] = lambda_vals_rest_A[:, None, None]
+
+    # For indices where the velocity is NaN - assume that it's zero
+    v_map[np.isnan(v_map)] = 0
+
+    # Min/max wavelength values taking into account the velocities in each spaxel
+    lambda_min_A = get_wavelength_from_velocity(lambda_rest_start_A, v_map, units="km/s")
+    lambda_max_A = get_wavelength_from_velocity(lambda_rest_stop_A, v_map, units="km/s")
+
+    # Indices within the desired wavelength window, after accounting for velocities in each spaxel
+    slice_mask = (lambda_vals_rest_A_cube > lambda_min_A) & (lambda_vals_rest_A_cube < lambda_max_A)
+
+    # Copies of datacubes with slices other than those in the wavelength window NaN'd out 
+    data_cube_masked = np.copy(data_cube)
+    data_cube_masked[~slice_mask] = np.nan
+    var_cube_masked = np.copy(var_cube)
+    var_cube_masked[~slice_mask] = np.nan
+
+    return data_cube_masked, var_cube_masked
+
+###############################################################################
+def compute_v_grad(v_map):
+    """Compute v_grad using eqn. 1 of Zhou+2017."""
+    v_grad = np.full_like(v_map, np.nan)
+    if v_map.ndim == 2:
+        ny, nx = v_map.shape
+        for yy, xx in product(range(1, ny - 1), range(1, nx - 1)):
+            v_grad[yy, xx] = np.sqrt(((v_map[yy, xx + 1] - v_map[yy, xx - 1]) / 2)**2 +\
+                                        ((v_map[yy + 1, xx] - v_map[yy - 1, xx]) / 2)**2)
+    elif v_map.ndim == 3:
+        ny, nx = v_map.shape[1:]
+        for yy, xx in product(range(1, ny - 1), range(1, nx - 1)):
+            v_grad[:, yy, xx] = np.sqrt(((v_map[:, yy, xx + 1] - v_map[:, yy, xx - 1]) / 2)**2 +\
+                                        ((v_map[:, yy + 1, xx] - v_map[:, yy - 1, xx]) / 2)**2)
+
+    return v_grad
