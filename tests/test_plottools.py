8b1bc86ec521cacb98ce060dbe425c1819f6fe05
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-08-18 12:20:11 +1000

    Removed old import of plottools_new from when I was debugging

--- tests/test_plottools.py
+++ tests/test_plottools.py
@@ -1,4 +1,4 @@
-from spaxelsleuth.plotting import plottools, plottools_new
+from spaxelsleuth.plotting import plottools
 
 cols = [
     "HALPHA",
@@ -14,43 +14,21 @@ cols = [
 
 # label function
 for col in cols:
-    # Old 
-    label = plottools.cmap_fn(col)
-    print(f"OLD: {col} --> {label}")
-
-    # New   
-    label = plottools_new.get_cmap(col)
-    print(f"NEW: {col} --> {label}")
+    label = plottools.get_cmap(col)
+    print(f"{col} --> {label}")
 
 # label function
 for col in cols:
-    # Old 
-    label = plottools.label_fn(col)
-    print(f"OLD: {col} --> {label}")
-
-    # New   
-    label = plottools_new.get_label(col)
-    print(f"NEW: {col} --> {label}")
+    label = plottools.get_label(col)
+    print(f"{col} --> {label}")
 
 # vmin/vmax functions
 for col in cols:
-    # Old 
-    vmin = plottools.vmin_fn(col)
-    vmax = plottools.vmax_fn(col)
-    print(f"OLD: {col} --> {vmin}, {vmax}")
-
-    # New   
-    vmin = plottools_new.get_vmin(col)
-    vmax = plottools_new.get_vmax(col)
-    print(f"NEW: {col} --> {vmin}, {vmax}")
-
+    vmin = plottools.get_vmin(col)
+    vmax = plottools.get_vmax(col)
+    print(f"{col} --> {vmin}, {vmax}")
 
 # Colname helper function
 for col in cols:
-    # Old 
-    c, s = plottools._colname_helper_fn(col)
-    print(f"OLD: {col} --> {c} + {s}")
-
-    # New   
-    c, s = plottools_new.trim_suffix(col)
-    print(f"NEW: {col} --> {c} + {s}")
+    c, s = plottools.trim_suffix(col)
+    print(f"{col} --> {c} + {s}")
