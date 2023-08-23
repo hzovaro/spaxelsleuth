48c4f0d44cd5e87dc539c304d9b920195342a1de
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-08-24 09:30:04 +1000

    Auto-formatted

--- tests/make_lzifu_test_df.py
+++ tests/make_lzifu_test_df.py
@@ -1,11 +1,16 @@
 import os
 
 from spaxelsleuth import load_user_config
+
 load_user_config("/home/u5708159/.spaxelsleuthconfig.json")
 from spaxelsleuth.loaddata.lzifu import make_lzifu_df
 from spaxelsleuth.config import settings
 
-gals = [g.rstrip("_R.fits.gz") for g in os.listdir(settings["lzifu"]["data_cube_path"]) if g.endswith("_R.fits.gz") and not g.startswith("._")]
+gals = [
+    g.rstrip("_R.fits.gz")
+    for g in os.listdir(settings["lzifu"]["data_cube_path"])
+    if g.endswith("_R.fits.gz") and not g.startswith("._")
+]
 kwargs = {
     "eline_SNR_min": 1,
     "sigma_gas_SNR_min": 1,
@@ -19,5 +24,12 @@ kwargs = {
     "correct_extinction": False,
     "bin_type": "default",
 }
-make_lzifu_df(gals=gals, ncomponents=1, sigma_inst_kms=29.6, nthreads=1, df_fname="test_lzifu.hd5", **kwargs)
-
+make_lzifu_df(gals=gals,
+              ncomponents=1,
+              sigma_inst_kms=29.6,
+              metallicity_diagnostics=[
+                  "N2Ha_PP04",
+              ],
+              nthreads=10,
+              df_fname="test_lzifu.hd5",
+              **kwargs)
