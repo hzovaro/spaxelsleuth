9f5b4a3d04cd4d992bc0142cc72285dffb5b8a5e
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-08-25 16:06:13 +1000

    Only runs on 3 galaxies to save time

--- tests/make_lzifu_test_df.py
+++ tests/make_lzifu_test_df.py
@@ -1,8 +1,8 @@
 import os
 
-from spaxelsleuth import load_user_config
-
+from spaxelsleuth import load_user_config, configure_logger
 load_user_config("/home/u5708159/.spaxelsleuthconfig.json")
+configure_logger(level="DEBUG")
 from spaxelsleuth.loaddata.lzifu import make_lzifu_df
 from spaxelsleuth.config import settings
 
@@ -10,7 +10,7 @@ gals = [
     g.rstrip("_R.fits.gz")
     for g in os.listdir(settings["lzifu"]["data_cube_path"])
     if g.endswith("_R.fits.gz") and not g.startswith("._")
-]
+][:3]
 kwargs = {
     "eline_SNR_min": 1,
     "sigma_gas_SNR_min": 1,
@@ -20,9 +20,7 @@ kwargs = {
     "flux_fraction_cut": False,
     "sigma_gas_SNR_cut": True,
     "vgrad_cut": False,
-    "stekin_cut": False,
     "correct_extinction": False,
-    "bin_type": "default",
 }
 make_lzifu_df(gals=gals,
               ncomponents=1,
@@ -30,6 +28,6 @@ make_lzifu_df(gals=gals,
               metallicity_diagnostics=[
                   "N2Ha_PP04",
               ],
-              nthreads=10,
+              nthreads=3,
               df_fname="test_lzifu.hd5",
               **kwargs)
