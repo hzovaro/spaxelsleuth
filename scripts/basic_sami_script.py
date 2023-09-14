2cff2c964bb24c0169cb84f035776c248bcf32af
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-09-14 12:26:14 +1000

    Tidied up script

--- scripts/basic_sami_script.py
+++ scripts/basic_sami_script.py
@@ -1,24 +1,25 @@
-import os
-from spaxelsleuth import load_user_config
-load_user_config("/home/u5708159/.spaxelsleuthconfig.json")
-from spaxelsleuth.loaddata.sami import make_sami_metadata_df, make_sami_df, load_sami_df, load_sami_metadata_df
+if __name__ == "__main__":
+    from spaxelsleuth import load_user_config
+    load_user_config("/Users/u5708159/Desktop/spaxelsleuth_test/.myconfig.json")
+    from spaxelsleuth.loaddata.sami import make_sami_metadata_df, make_sami_df, load_sami_metadata_df, load_sami_df
 
-DEBUG = True
+    nthreads = 4
+    DEBUG = True
 
-# Create the DataFrames
-make_sami_metadata_df(nthreads=os.cpu_count())
-make_sami_df(bin_type="default", 
-             ncomponents="recom", 
-             eline_SNR_min=5, 
-             correct_extinction=True,
-             metallicity_diagnostics=["R23_KK04"],
-             nthreads=os.cpu_count(),
-             debug=DEBUG)
+    # Create the DataFrames
+    make_sami_metadata_df(recompute_continuum_SNRs=True, nthreads=nthreads)
+    make_sami_df(bin_type="default", 
+                ncomponents="recom", 
+                eline_SNR_min=5, 
+                correct_extinction=True,
+                metallicity_diagnostics=["R23_KK04"],
+                nthreads=nthreads,
+                debug=DEBUG)
 
-# Load the DataFrames
-df_metadata = load_sami_metadata_df()
-df = load_sami_df(ncomponents="recom",
-                  bin_type="default",
-                  eline_SNR_min=5,
-                  correct_extinction=True,
-                  debug=DEBUG)
+    # Load the DataFrames
+    df_metadata = load_sami_metadata_df()
+    df = load_sami_df(ncomponents="recom",
+                    bin_type="default",
+                    eline_SNR_min=5,
+                    correct_extinction=True,
+                    debug=DEBUG)
