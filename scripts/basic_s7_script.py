8d8b50eaa6869daecde3b21566839fd3c3008460
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-11-28 14:54:58 +1100

    Now creates/loads the DataFrame for all S7 galaxies instead of a subset

--- scripts/basic_s7_script.py
+++ scripts/basic_s7_script.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
         load_user_config("/home/u5708159/.spaxelsleuthconfig.json")
     from spaxelsleuth.loaddata.s7 import make_s7_metadata_df, make_s7_df, load_s7_metadata_df, load_s7_df
 
-    nthreads = 4
+    nthreads = 10
     eline_SNR_min = 3
     eline_ANR_min = 3
 
     # Create the DataFrames
     make_s7_metadata_df()
-    make_s7_df(gals=["NGC1068", "MARK573", "NGC6860"],
+    make_s7_df(
                 eline_SNR_min=eline_SNR_min,
                 eline_ANR_min=eline_ANR_min,
                 correct_extinction=True,
