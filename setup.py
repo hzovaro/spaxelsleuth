07798888aecd4e0393fbbba458f62ad4ee36179e
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-11-29 14:47:33 +1100

    Fixed installation issue where packages weren't being loaded

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 """
 Set up for spaxelsleuth.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 setup(
     name="spaxelsleuth",
-    packages=["spaxelsleuth"],
+    packages=find_packages(),
     version="0.9.1",
     # James: all I need is for people to make some kind of env w/ python > 3.10, and then install via pip install .
     # Don't need to faff around w/ conda environments.
