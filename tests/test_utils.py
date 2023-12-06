fb1b188a8575e076c4d514f3ff35807128613ad4
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-12-06 23:30:53 +1100

    Added trivial tests:

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+import numpy as np
+
+from spaxelsleuth.utils import continuum
+
+# Load or create a dummy DataFrame
+
+def test_compute_d4000():
+    assert True
+
+def test_compute_continuum_intensity():
+    assert True
+
+def test_compute_continuum_luminosity():
+    assert True
+
+def test_compute_EW():
+    assert True
---
+++ tests/test_velocity.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import numpy as np
+
+from spaxelsleuth.utils import velocity
+
+def test_get_wavelength_from_velocity():
+    assert True
+
+def test_get_slices_in_velocity_range():
+    assert True
+
+def test_compute_v_grad():
+    assert True
+
