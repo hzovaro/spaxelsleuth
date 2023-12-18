58c2b74044022957d29171bc081153534db7b82f
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-12-18 16:27:33 +1100

    Added ChatGPT docstrings

--- spaxelsleuth/utils/velocity.py
+++ spaxelsleuth/utils/velocity.py
@@ -7,7 +7,31 @@ logger = logging.getLogger(__name__)
 
 ###############################################################################
 def get_wavelength_from_velocity(lambda_rest, v, units):
-    """Compute the Doppler-shifted wavelength given a velocity and a rest-frame wavelength."""
+    """Compute the Doppler-shifted wavelength corresponding to a velocity and a rest-frame wavelength.
+
+    Note that the input velocity value MUST be relative to the systemic velocity of the galaxy,
+    i.e. must not include the cz term.
+    
+    The returned wavelength value is determined using the Doppler formula, i.e.
+
+                    nu_0^2 - nu^2
+        velocity =  ______________
+                    
+                    nu_0^2 + nu^2
+
+    where nu_0 is the rest-frame frequency and nu is the observer-frame frequency. 
+
+    See https://science.nrao.edu/facilities/vla/docs/manuals/obsguide/modes/line. 
+
+    Parameters:
+    - lambda_rest (float): Rest wavelength of the spectral line.
+    - v (float): Velocity of the source along the line of sight.
+    - units (str): Units of velocity, must be 'm/s' or 'km/s'.
+
+    Returns:
+    - lambda_obs (float): Observed wavelength of the spectral line, in the same units as lambda_rest.
+
+    """
     if units not in ['m/s', 'km/s',]:
         raise ValueError("units must be m/s or km/s!")
     if units == 'm/s':
@@ -21,7 +45,25 @@ def get_wavelength_from_velocity(lambda_rest, v, units):
 
 ###############################################################################
 def get_slices_in_velocity_range(data_cube, var_cube, lambda_vals_rest_A, lambda_rest_start_A, lambda_rest_stop_A, v_map):
-    """Returns a copy of the data/variance cubes with slices outside those in a specified wavelength range masked out."""
+    """Returns a copy of the data and variance cubes with slices outside a specified wavelength range masked out based on the velocity map.
+
+    Parameters:
+    - data_cube (numpy.ndarray): 3D array representing the data cube.
+    - var_cube (numpy.ndarray): 3D array representing the variance cube.
+    - lambda_vals_rest_A (numpy.ndarray): 1D array containing rest wavelength values.
+    - lambda_rest_start_A (float): Start wavelength of the desired range in Angstroms.
+    - lambda_rest_stop_A (float): Stop wavelength of the desired range in Angstroms.
+    - v_map (numpy.ndarray): 2D array representing the velocity map in km/s.
+
+    Returns:
+    - data_cube_masked, var_cube_masked (numpy.ndarray): copies of the input data 
+      and variance cubes with slices outside the specified velocity range NaN'd out.
+
+    Note:
+    The velocity map is used to calculate the corresponding velocity-dependent wavelength range.
+    For indices where the velocity is NaN, it is assumed to be zero.
+
+    """
     # 3D array containing wavelength values in each spaxel
     lambda_vals_rest_A_cube = np.zeros(data_cube.shape)
     lambda_vals_rest_A_cube[:] = lambda_vals_rest_A[:, None, None]
@@ -46,7 +88,18 @@ def get_slices_in_velocity_range(data_cube, var_cube, lambda_vals_rest_A, lambda
 
 ###############################################################################
 def compute_v_grad(v_map):
-    """Compute v_grad using eqn. 1 of Zhou+2017."""
+    """Computes the gradient of the velocity map using central differencing as per eqn. 1 of Zhou+2017.
+
+    Parameters:
+    - v_map (numpy.ndarray): 2D or 3D array representing the velocity map.
+
+    Returns:
+    - v_grad (numpy.ndarray): Array containing the magnitude of the velocity gradient at each point.
+
+    Note:
+    - For a 2D velocity map, central differencing is applied in both x and y directions.
+    - For a 3D velocity map, central differencing is applied along the spatial dimensions.
+"""
     logger.debug("computing velocity gradients...")
     v_grad = np.full_like(v_map, np.nan)
     if v_map.ndim == 2:
