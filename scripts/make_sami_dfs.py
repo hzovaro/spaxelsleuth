9c7106bc57945f487c4d702f85a5340c49b47c00
Henry Zovaro <henry.zovaro@anu.edu.au>
2023-12-21 17:37:16 +1100

    Added eline_ANR_min args

--- scripts/make_sami_dfs.py
+++ scripts/make_sami_dfs.py
@@ -15,33 +15,33 @@ make_sami_metadata_df(recompute_continuum_SNRs=False, nthreads=nthreads)
 # Create the DataFrames
 ###########################################################################
 # Make test data 
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=True, nthreads=nthreads)
 
 # Make full data set
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, correct_extinction=True, debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, correct_extinction=True, debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, correct_extinction=True, debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="1", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="adaptive", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="1", bin_type="adaptive", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="sectors", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="1", bin_type="sectors", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, debug=False, nthreads=nthreads)
 
 # Make LZIFU data set 
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="1", debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="3", debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="recom", debug=True, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="1", debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="3", debug=False, nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="recom", debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="1", debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="3", debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="recom", debug=True, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="1", debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="3", debug=False, nthreads=nthreads)
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="recom", debug=False, nthreads=nthreads)
 
 # Make LZIFU data set WITHOUT S/N or DQ cuts 
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=False,
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=True, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=False,
              line_flux_SNR_cut=False,
              missing_fluxes_cut=False,
              line_amplitude_SNR_cut=False,
@@ -50,7 +50,7 @@ make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_e
              vgrad_cut=False,
              stekin_cut=False,
              nthreads=nthreads)
-make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, correct_extinction=False, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=False,
+make_sami_df(ncomponents="recom", bin_type="default", eline_SNR_min=5, eline_ANR_min=3, correct_extinction=False, __use_lzifu_fits=True, __lzifu_ncomponents="2", debug=False,
              line_flux_SNR_cut=False,
              missing_fluxes_cut=False,
              line_amplitude_SNR_cut=False,
