527abf25eceb5d0afad024d5a77fbfd8f4b0260d
Henry Zovaro <henry.zovaro@anu.edu.au>
2024-02-06 17:03:54 +1100

    Updated to work with new boilerplate I/O functions; runs without crashing

--- scripts/basic_hector_script.py
+++ scripts/basic_hector_script.py
@@ -7,28 +7,32 @@ if __name__ == "__main__":
         load_user_config("/Users/u5708159/Desktop/spaxelsleuth_test/.myconfig.json")
     except FileNotFoundError:
         load_user_config("/home/u5708159/.spaxelsleuthconfig.json")
-    from spaxelsleuth.io.hector import make_hector_metadata_df, make_hector_df, load_hector_metadata_df, load_hector_df
+    from spaxelsleuth.io.io import make_metadata_df, make_df, load_metadata_df, load_df
     from spaxelsleuth.plotting.plot2dmap import plot2dmap
 
-    nthreads = 1
+    nthreads = 4
 
     # Create the DataFrames
-    make_hector_metadata_df()
-    df_metadata = load_hector_metadata_df()
+    make_metadata_df(survey="hector")
+    df_metadata = load_metadata_df(survey="hector")
     
-    gals = df_metadata.index.values[:10]
+    gals = df_metadata.index.values[:min(df_metadata.shape[0], 10)]
 
     # Load the DataFrames
-    make_hector_df(ncomponents="rec", 
-                eline_SNR_min=5, 
-                eline_ANR_min=3, 
-                correct_extinction=True,
-                metallicity_diagnostics=["N2Ha_K19"],
-                gals=gals,
-                nthreads=nthreads)
+    make_df(survey="hector", 
+            bin_type="default",
+            ncomponents="rec", 
+            eline_SNR_min=5, 
+            eline_ANR_min=3, 
+            correct_extinction=True,
+            metallicity_diagnostics=["N2Ha_K19"],
+            gals=gals,
+            nthreads=nthreads)
 
-    df = load_hector_df(ncomponents="rec",
-                    eline_SNR_min=5,
-                    eline_ANR_min=3,
-                    correct_extinction=True)
+    df = load_df(survey="hector", 
+                 bin_type="default",
+                 ncomponents="rec",
+                 eline_SNR_min=5,
+                 eline_ANR_min=3,
+                 correct_extinction=True)
 
