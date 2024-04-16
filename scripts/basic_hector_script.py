e349ac4f77e2c617627ee6274d4bcae30aa55800
Henry Zovaro <henry.zovaro@anu.edu.au>
2024-04-17 09:53:53 +1000

    Updated to reflect changes to make/load_df

--- scripts/basic_hector_script.py
+++ scripts/basic_hector_script.py
@@ -15,24 +15,21 @@ if __name__ == "__main__":
     # Create the DataFrames
     make_metadata_df(survey="hector")
     df_metadata = load_metadata_df(survey="hector")
-    
-    gals = df_metadata.index.values[:min(df_metadata.shape[0], 10)]
 
     # Load the DataFrames
     make_df(survey="hector", 
             bin_type="default",
             ncomponents="rec", 
-            eline_SNR_min=5, 
+            eline_SNR_min=3, 
             eline_ANR_min=3, 
             correct_extinction=True,
             metallicity_diagnostics=["N2Ha_K19"],
-            gals=gals,
             nthreads=nthreads)
 
-    df = load_df(survey="hector", 
+    df, ss_params = load_df(survey="hector", 
                  bin_type="default",
                  ncomponents="rec",
-                 eline_SNR_min=5,
+                 eline_SNR_min=3,
                  eline_ANR_min=3,
                  correct_extinction=True)
 
